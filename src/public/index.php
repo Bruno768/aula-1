a9c3f9e4822cad6e2e095d1c42b97fbdc524ac6c
ukiina <igormorais85@gmial.com>
2023-04-26 11:12:29 -0300

    altereção commit

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,40 @@
 <?php
 
-phpinfo();
+//phpinfo();
+//declare(strict_types=1);
+$nome = 'Maria';
+$idade = 30;
+
+echo 'a: ['. 73 . ']<br/>';
+echo 'b: ['. 'oi' .']<br/>';
+echo 'c: ['. false .']<br/>';
+echo 'd: ['. $nome .']<br/>';
+echo 'e: ['. $idade .']<br/>';
+//
+%saldo = 10;
+if ($saldo,100){
+    $deposito = 1000;
+    $saldo += $deposito;
+}
+
+echo $saldo;
+
+
+echo 'm: ['.true.']<br />';
+echo 'i: [' . false . ']<br/>';
+
+$time = array ('maria','jose','ana');
+echo $time[2];
+echo '<br />';
+print_r($time);
+
+$contador = 1;
+$mensagem = "ola";
+$versao = array('1.1','2','3.4');
+print_r($versao);
+
+$primeiro_nome ="Maria";
+echo $primeiro_nome;
+echo '<br />';
+$ultimo_nome = "lourdes";
+echo $ultimo_nome;
